c23d7a6bbdb9ff2cf4d80624c64d08b95bbf32f9
Ye Chong <42334254+yechong316@users.noreply.github.com>
2019-04-05 02:47:01 +0800

    Add files via upload
    开始尝试自己第一个机器学习项目

---
+++ 线性回归项目之低能耗家庭用电回归评估/低能耗家电电源回归模型预测.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+
+# mpl.rcParams['fonts'] = [u'senHei']
+
+# ######################################
+# 加载数据以及数据清洗
+# ######################################
+df = pd.read_csv("D:\人工智能\数据集\energydata_complete_1000.csv")
+print(df.info())
+
+# ######################################
+# 构造特征工程
+# ######################################
+
+# ######################################
+# 开始训练
+# ######################################
+
+# ######################################
+# 模型展示
+# ######################################
