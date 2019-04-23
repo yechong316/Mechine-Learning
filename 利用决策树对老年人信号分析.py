c9d007ab49ccd13a0beef39b260de7fc5415c79f
Ye Chong <42334254+yechong316@users.noreply.github.com>
2019-04-23 22:08:32 +0800

    Create 利用决策树对老年人信号分析.py
    基于决策树对老年人信号评估

---
+++ 利用决策树对老年人信号分析.py
@@ -0,0 +1,68 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import os
+from ML.linear_regression import R2_square
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+
+# 数据加载
+names = [
+    'Time_second', 'frontal_axis', 'vertical_axis',
+    'lateral_axis', 'Id', 'RSSI', 'Phase', 'Frequency', 'activity',
+]
+
+feature = [
+    'Time_second', 'frontal_axis', 'vertical_axis',
+    'lateral_axis', 'Id', 'RSSI', 'Phase', 'Frequency'
+]
+filefold_path = '../datas/S1_Dataset'
+# print(os.listdir(filefold_path))
+
+data_path = [
+    filefold_path + '/' + i for i in os.listdir(filefold_path)
+]
+df = [
+pd.read_csv(i, header=None, names=names,sep=',') for i in data_path
+]
+df = pd.concat(df)
+df.replace('?', np.nan, inplace=True)
+df = df.dropna(axis=0, how='any')
+
+# 数据分割
+X = df.iloc[:, 0:-1]
+Y = df.iloc[:, -1]
+x_train, x_test, y_train, y_test = train_test_split(X, Y, random_state=20)
+
+# 建立模型
+algo = DecisionTreeClassifier()
+algo.fit(x_train, y_train)
+y_pred = algo.predict(x_test)
+
+# 模型预测
+print('训练集误差为：', algo.score(x_train, y_train))
+print('测试集误差为：', algo.score(x_test, y_test))
+
+# 模型展示
+t = np.arange(len(x_test))
+
+plt.plot(t, y_pred, 'r')
+plt.plot(t, y_test, 'b')
+
+# b. 可视化方式二：直接使用pydotpuls库将数据输出为pdf或者png格式
+from sklearn import tree
+import pydotplus
+
+dot_data = tree.export_graphviz(decision_tree=algo, out_file=None,
+                                feature_names=feature,
+                                class_names=['sit on bed', 'sit on chair', 'lying', 'ambulating'],
+                                filled=True, rounded=True,
+                                special_characters=True
+                                )
+
+graph = pydotplus.graph_from_dot_data(dot_data)
+graph.write_png('older.png')
+graph.write_pdf('older.pdf')
